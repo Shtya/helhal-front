c89af84f569e41cc50b528205176473de7efc86e
shtya <shtya54@gmail.com>
2025-10-21 14:08:38 +0300

    init

--- requirments.js
+++
@@ -1,8 +0,0 @@
-/*
-
-	- github frontend :  https://github.com/Shtya/helhal-front
-	- github backend : https://github.com/Shtya/helhal-back
-
-	- https://www.figma.com/design/NHSS9qrHhHmujVhfgFaGcu/Helhal--Alhanouf-?node-id=91-5124&p=f&t=YigNkcfpL2ndiATv-0
-
-*/
