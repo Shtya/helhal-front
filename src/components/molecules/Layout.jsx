d9607aa4b41a10be69b3336e362e6fd63208137f
amagdykhalil <am2592379@gmai.com>
2025-10-26 00:12:35 +0300

    Add progress indicator for route change

--- src/components/molecules/Layout.jsx
+++ src/components/molecules/Layout.jsx
@@ -2,9 +2,10 @@
 import { Toaster } from 'react-hot-toast';
 import { GlobalProvider } from '../../context/GlobalContext';
 import Header from './Header';
-import { usePathname, useRouter } from '../../i18n/navigation';
+import { usePathname } from '../../i18n/navigation';
 import { Footer } from './Footer';
 import ConfigAos from '@/config/Aos';
+import { ProgressProvider } from '@bprogress/next/app';
 
 export default function Layout({ children, params }) {
   const pathname = usePathname();
@@ -13,11 +14,18 @@ export default function Layout({ children, params }) {
 
   return (
     <GlobalProvider>
-      {!isAuthRoute && <Header />}
-      <div className='  ' >{children}</div>
-      {!isAuthRoute && <Footer />}
-      <ConfigAos />
-      <Toaster position='top-center' />
+      <ProgressProvider
+        height="2px"
+        color="#0070f3"
+        options={{ showSpinner: false }}
+        shallowRouting
+      >
+        {!isAuthRoute && <Header />}
+        <div className='  ' >{children}</div>
+        {!isAuthRoute && <Footer />}
+        <ConfigAos />
+        <Toaster position='top-center' />
+      </ProgressProvider>
     </GlobalProvider>
   );
 }
