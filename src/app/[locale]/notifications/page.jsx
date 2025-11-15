a9185bcc6efd4834ee03cedbe8e84c2e321e5ded
amagdykhalil <am2592379@gmai.com>
2025-11-15 15:11:22 +0200

    Update notification loading style

--- src/app/[locale]/notifications/page.jsx
+++ src/app/[locale]/notifications/page.jsx
@@ -163,7 +163,7 @@ const NotificationsPage = () => {
 
   if (loading && pageNotifications.length === 0) {
     return (
-      <div className='container mx-auto px-4 py-8 max-w-4xl'>
+      <div className='container mx-auto px-4 py-8 max-w-4xl !mt-6'>
         <div className='animate-pulse space-y-4'>
           <div className='h-8 bg-gray-200 rounded w-1/4'></div>
           {[1, 2, 3, 4, 5].map(i => (
